39fad8230ab3cf67ae872169b762f1270805a069
aiscun <aiscundok@gmail.com>
2019-08-23 08:12:01 +0800

    Update customer.php

--- customer.php
+++ customer.php
@@ -23,7 +23,7 @@ $customers = query("SELECT * FROM customer");
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Network Monitoring System | PT Indonesia Comnets Plus</title>
+  <title>Network Monitoring System</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -170,7 +170,7 @@ $customers = query("SELECT * FROM customer");
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
           	<p>Copyright © </p>
-            <a href="http://www.iconpln.co.id"><span>PT Indonesia Comnets Plus</span></a>
+           
           </div>
         </div>
       </footer>
