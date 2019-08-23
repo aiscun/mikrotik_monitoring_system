186e6d550cec04895bca30dcb0979460ee0c8958
aiscun <aiscundok@gmail.com>
2019-08-23 08:30:30 +0800

    Update pop.php

--- pop.php
+++ pop.php
@@ -26,7 +26,7 @@ if (isset($_POST["cari"])) {
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Network Monitoring System | PT Indonesia Comnets Plus</title>
+  <title>Network Monitoring System</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -188,7 +188,6 @@ if (isset($_POST["cari"])) {
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
           	<p>Copyright © </p>
-            <a href="http://www.iconpln.co.id"><span>PT Indonesia Comnets Plus</span></a>
           </div>
         </div>
       </footer>
